de9f8c42680c1382e534906789cd06701545a231
waleedumer <waleedumer42@outlook.com>
2017-04-10 18:01:54 +0500

    Update server.js

--- server.js
+++ server.js
@@ -85,12 +85,12 @@ app.configure(function() {
 });
 
 app.get('/', function(req, res) {
-    res.render('single', {json: json});
+    res.render('index', {json: json});
 
 });
 
 app.get('/a', function(req, res) {
-    res.render('index', {json: json});
+    res.render('single', {json: json});
 
 });
 
