fcafbb4693c954fc288d2339d89c28311e23cca7
waleedumer <waleedumer42@outlook.com>
2017-04-10 16:07:17 +0500

    Change to check

--- server.js
+++ server.js
@@ -83,14 +83,17 @@ app.configure(function() {
     app.use(express.cookieParser());
     app.use(express.static('static'));
 });
-
+if(req.url === '/single.html')
+{
+  res.wri
+}
 app.get('/', function(req, res) {
     res.render('index', {json: json});
 
 });
 
 app.get('/single.html', function (req, res) {
-  res.send('single.ejs')
+  res.send('Hi How ate')
 });
 
 
